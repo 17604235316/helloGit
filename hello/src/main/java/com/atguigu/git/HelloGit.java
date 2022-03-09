6ccdccd853a8bbdbc49c501bfaecf41db8d18e95
YS <810317099@qq.com>
2022-03-09 21:54:28 +0800

    创建分支

--- hello/src/main/java/com/atguigu/git/HelloGit.java
+++ hello/src/main/java/com/atguigu/git/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("update1");
+        System.out.println("创建分支");
     }
 }
